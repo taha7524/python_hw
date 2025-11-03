437f64bbda22f5f16e83366d8989e4de4ec060a2
taha7524 <nenataha.75@gmail.com>
2025-11-03 22:05:00 +0400

    add lesson_3

---
+++ all_praktik/03_lesson_praktik.py
@@ -0,0 +1,10 @@
+class User:
+
+	def __init__(self, name): 
+		print("я создался")
+		print('меня зовут', name)
+		
+
+user1 = User("Alex")
+user2 = User("Mark")
+user3 = User("Marta")
---
+++ all_praktik/product.py
@@ -0,0 +1,8 @@
+class Product:
+    def__init__(self, name, price):
+        self. name = name
+        self. prise = prise
+    
+    def get_name(self):
+        return self.name
+
