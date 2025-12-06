ff81e8ac9220c5cd10f9b5c443212efa4baba153
taha7524 <nenataha.75@gmail.com>
2025-12-06 18:57:52 +0400

    add lesson06

---
+++ current_url.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+browser = webdriver.Chrome(
+service=ChromeService(ChromeDriverManager().install()))
+
+browser.get("http://ya.ru/")
+url = browser.current_url
+print(url)
+
+browser.quit()
